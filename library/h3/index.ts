f0f1d783dad14632fb93d57e2977180a0fb1e0fe
Thomas Juster <juster.thomas@gmail.com>
2024-09-11 09:55:57 +0200

    h3: install & implement server handler adapter

---
+++ library/h3/index.ts
@@ -0,0 +1,47 @@
+import { defineEventHandler, getHeader, sendRedirect, setHeader } from "h3"
+import { HttpError, NotFound } from "library/std/http-error"
+import { MimeType } from "library/std/mime-type"
+import { isRedirect, type Handler } from "library/std/server-handler"
+import { tryOr } from "library/std/try-or"
+
+function ResponseFromHttpError(error: HttpError) {
+  const body = tryOr(
+    () => JSON.stringify(error),
+    () => JSON.stringify({ message: error.message }),
+  )
+  return new Response(body, {
+    status: error.code,
+    headers: { "Content-Type": MimeType.Json },
+  })
+}
+
+export { createApp, createRouter, toNodeListener } from "h3"
+
+export function defineHandler(handler: Handler) {
+  return defineEventHandler(async (event) => {
+    const method = event.method.toLowerCase()
+    if (method !== "get" && method !== "post")
+      return ResponseFromHttpError(
+        NotFound({ message: `Unsupported method: ${method}` }),
+      )
+
+    const result = await handler({
+      method,
+      url: new URL(event.path, "http://localhost"),
+      params: event.context.params ?? {},
+      // TODO: provide the body
+      body: undefined,
+      getHeader: (name) => getHeader(event, name),
+      setHeader: (name, value) => setHeader(event, name, value),
+    })
+
+    if (result instanceof HttpError) return ResponseFromHttpError(result)
+
+    if (isRedirect(result))
+      return sendRedirect(event, result.location.href, result.code)
+
+    return new Response(result.body, {
+      headers: { "Content-Type": result.type },
+    })
+  })
+}
